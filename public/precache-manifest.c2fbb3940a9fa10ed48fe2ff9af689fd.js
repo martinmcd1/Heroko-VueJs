29b5634ec1b3e674da62b933d8a1f1c3ea3213cc
Martin MacDonald <martin.macdonald@screenmedia.co.uk>
2018-04-30 22:14:03 +0100

    first commit

---
+++ public/precache-manifest.c2fbb3940a9fa10ed48fe2ff9af689fd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a114a901ad3e7fc2b0e3",
+    "url": "/js/vendor.a114a901.js"
+  },
+  {
+    "revision": "bb41d6d80c1fdaad6874",
+    "url": "/js/manifest.bb41d6d8.js"
+  },
+  {
+    "revision": "c52d3b4247f359a3c998",
+    "url": "/js/app.c52d3b42.js"
+  },
+  {
+    "revision": "db223758d4d7ec244f9580ceb8114637",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c52d3b4247f359a3c998",
+    "url": "/css/app.ff401333.css"
+  }
+];
